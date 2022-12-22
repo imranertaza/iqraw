effccb7e09cfcef09a235c9bd2246308a438ae39
dnationsoft123 <dnationsoftdm8@gmail.com>
2022-12-22 10:39:47 +0600

    gg

--- app/Config/App.php
+++ app/Config/App.php
@@ -25,7 +25,7 @@ class App extends BaseConfig
      * @var string
      */
 //    public $baseURL = 'http://localhost:8080/';
-    public $baseURL = 'http://iqraw.dn/';
+    public $baseURL = 'http://iqraw.dns/';
 
     /**
      * --------------------------------------------------------------------------
