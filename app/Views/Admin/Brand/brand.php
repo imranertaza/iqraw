140863407468baf78aef51b12a7f944867608ce5
dnationsoft123 <dnationsoftdm8@gmail.com>
2023-01-01 20:11:06 +0600

    brand

--- app/Views/Admin/Brand/brand.php
+++ app/Views/Admin/Brand/brand.php
@@ -5,7 +5,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Store</h1>
+                    <h1>Brand</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
