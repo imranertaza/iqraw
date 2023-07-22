e5609da006cd460c83c40dcf7ed0e3e0f30eaa93
Syed Imran Ertaza <imranertaza12@gmail.com>
2023-07-22 11:09:31 +0600

    cache cleaned with gitignore

--- vendor/rector/rector/vendor/react/promise-timer/src/functions_include.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace RectorPrefix20220531\React\Promise\Timer;
-
-if (!\function_exists('RectorPrefix20220531\\React\\Promise\\Timer\\timeout')) {
-    require __DIR__ . '/functions.php';
-}
--- vendor/rector/rector/vendor/symplify/easy-parallel/src/Exception/ParallelShouldNotHappenException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare (strict_types=1);
-namespace RectorPrefix20220531\Symplify\EasyParallel\Exception;
-
-use Exception;
-final class ParallelShouldNotHappenException extends \Exception
-{
-}
