69c2046263d846619630d48e0f635deaff9ee6e6
Syed Imran Ertaza <imran@dnationsoft.com>
2023-08-02 19:40:15 +0600

    gitignore file updated

--- vendor/rector/rector/packages/BetterPhpDocParser/Contract/BasePhpDocNodeVisitorInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare (strict_types=1);
-namespace Rector\BetterPhpDocParser\Contract;
-
-use RectorPrefix20220531\Symplify\Astral\PhpDocParser\Contract\PhpDocNodeVisitorInterface;
-interface BasePhpDocNodeVisitorInterface extends \RectorPrefix20220531\Symplify\Astral\PhpDocParser\Contract\PhpDocNodeVisitorInterface
-{
-}
